e9b5a8b521271d6a62c3411c1391065fb18bccec
Austin <austinmeyer1106@gmail.com>
2022-10-03 14:41:09 -0500

    fixed a bug

--- api/posts.js
+++ api/posts.js
@@ -59,29 +59,6 @@ postsRouter.post('/', requireActiveUser, async (req, res, next) => {
     }
 });
 
-postsRouter.delete('/:postId', requireActiveUser, async (req, res, next) => {
-    try {
-        const post = await getPostById(req.params.postId);
-    
-        if (post && post.author.id === req.user.id) {
-            const updatedPost = await updatePost(post.id, { active: false });
-    
-            res.send({ post: updatedPost });
-        } else {
-            next(post ? { 
-            name: "UnauthorizedUserError",
-            message: "You cannot delete a post which is not yours"
-            } : {
-            name: "PostNotFoundError",
-            message: "That post does not exist"
-            });
-        }
-  
-    } catch ({ name, message }) {
-        next({ name, message })
-    }
-});
-
 postsRouter.patch('/:postId', requireActiveUser, async (req, res, next) => {
     const { postId } = req.params;
     const { title, content, tags } = req.body;
@@ -117,6 +94,27 @@ postsRouter.patch('/:postId', requireActiveUser, async (req, res, next) => {
     }
 });
 
-
+postsRouter.delete('/:postId', requireActiveUser, async (req, res, next) => {
+    try {
+        const post = await getPostById(req.params.postId);
+    
+        if (post && post.author.id === req.user.id) {
+            const updatedPost = await updatePost(post.id, { active: false });
+    
+            res.send({ post: updatedPost });
+        } else {
+            next(post ? { 
+            name: "UnauthorizedUserError",
+            message: "You cannot delete a post which is not yours"
+            } : {
+            name: "PostNotFoundError",
+            message: "That post does not exist"
+            });
+        }
+  
+    } catch ({ name, message }) {
+        next({ name, message })
+    }
+});
 
 module.exports = postsRouter;
